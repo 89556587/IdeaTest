e2a63039d91371b5ea210a98c421a6c26149bccd
qinwenzhuang <89556587@qq.com>
2020-05-25 15:51:36 +0800

    添加web模块，并添加用户业务层！

---
+++ webDemo/src/com/renhe/java/UserService.java
@@ -0,0 +1,13 @@
+package com.renhe.java;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 作者：QWZ
+ * 用户业务接口层
+ */
+public interface UserService {
+
+    List<Map<String, Object>> listUser();
+}
---
+++ webDemo/src/com/renhe/java/com/renhe/java/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.renhe.java.com.renhe.java.impl;
+
+import com.renhe.java.UserService;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 作者：QWZ
+ * 用户业务实现层
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<Map<String, Object>> listUser() {
+        return null;
+    }
+}
